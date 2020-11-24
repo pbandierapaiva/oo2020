97161c3974539a6e765dfd91763a32b37ad634a2
PAULO BANDIERA PAIVA <paiva@unifesp.br>
2020-11-24 13:48:06 -0300

    aula 24/11/20

---
+++ alo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#
+# Exemplo em classe
+# Autor: Paulo Bandiera Paiva
+#
+
+print('''
+		
+		
+		
+		Alo mundo!\n
+		
+		
+		com várias linhas
+		
+		...........
+		   ...........                               ...........
+		       ...........                   ...........    
+		             ...........     ...........
+		                      ...........
+                              
+                              
+		''')
+
